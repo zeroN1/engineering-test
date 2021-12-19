bf56d498842891845534268d53097a473c107e2b
allen-bware <allen@boardingware.com>
2021-12-19 19:22:46 +1300

    Reduce http mock timeout time

--- front-end/src/shared/helpers/http-mock.ts
+++ front-end/src/shared/helpers/http-mock.ts
@@ -6,13 +6,13 @@ interface Options {
 }
 export function httpMock({ success, randomFailure }: Options) {
   return new Promise<void>((resolve, reject) => {
-    // resolves randomly between 500ms to 2000ms
+    // resolves randomly between 200ms to 1000ms
     setTimeout(() => {
       if ((randomFailure && Math.random() < 0.99) || success) {
         resolve()
       } else {
         reject({ message: "Failed" })
       }
-    }, getRandomInt(5, 10) * 100)
+    }, getRandomInt(2, 10) * 100)
   })
 }
